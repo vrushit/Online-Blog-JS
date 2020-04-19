73d220f87a6e96bb34098c6a065eeb552aff35ea
vrushit <bharuchavrushit@ymail.com>
2020-04-20 01:45:50 +0530

    Nav Fixes

--- js/navBar.js
+++ js/navBar.js
@@ -7,9 +7,6 @@ const navSlide = () => {
     burger.addEventListener("click", () => {
         nav.classList.toggle('nav-active');
 
-
-        
-
     navLinks.forEach((link, index) => {
 
         if(link.style.animation)
@@ -18,21 +15,10 @@ const navSlide = () => {
         }
         else{
             link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
-
         }
     });
-
     burger.classList.toggle('toggle');
-
-
-
     });
-
-
-
-
-
-
 }
 
 navSlide();
